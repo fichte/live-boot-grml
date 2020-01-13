abae95e4ef20c78becba18d6f7fe963e3f6c896e
Torsten Fichtner <torsten@bashpipe.de>
2020-01-13 09:58:15 +0100

    support completely static ipv4/ipv6 configuration at initramfs stage
    * support dhcp/slaac independent boots in grml

--- components/9990-netbase.sh
+++ components/9990-netbase.sh
@@ -2,6 +2,36 @@
 
 #set -e
 
+Parseipv4 ()
+{
+	parsed=$(echo "${STATICIP}" | sed -e 's|,| |g')
+
+	for ifline in ${parsed}
+	do
+		ifname="$(echo ${ifline} | cut -f1 -d ':')"
+		ifaddress="$(echo ${ifline} | cut -f2 -d ':')"
+		ifnetmask="$(echo ${ifline} | cut -f3 -d ':')"
+		ifgateway="$(echo ${ifline} | cut -f4 -d ':')"
+		nameserver="$(echo ${ifline} | cut -f5 -d ':')"
+	done
+
+	ifprefix=$(ipcalc -p ${ifaddress} ${ifnetmask} | sed 's/PREFIX=//')
+}
+
+Parseipv6 ()
+{
+	parsed=$(echo "${STATICIP6}" | sed -e 's|,| |g')
+
+	for ifline in ${parsed}
+	do
+		ifname="$(echo ${ifline} | cut -f1 -d ';')"
+		ifaddress="$(echo ${ifline} | cut -f2 -d ';')"
+		ifprefix="$(echo ${ifline} | cut -f3 -d ';')"
+		ifgateway="$(echo ${ifline} | cut -f4 -d ';')"
+		nameserver="$(echo ${ifline} | cut -f5 -d ';')"
+	done
+}
+
 Netbase ()
 {
 	if [ -n "${NONETWORKING}" ]
@@ -37,15 +67,8 @@ EOF
 
 	if [ -z "${NETBOOT}" ] && [ -n "${STATICIP}" ] && [ "${STATICIP}" != "frommedia" ]
 	then
-		parsed=$(echo "${STATICIP}" | sed -e 's|,| |g')
 
-		for ifline in ${parsed}
-		do
-			ifname="$(echo ${ifline} | cut -f1 -d ':')"
-			ifaddress="$(echo ${ifline} | cut -f2 -d ':')"
-			ifnetmask="$(echo ${ifline} | cut -f3 -d ':')"
-			ifgateway="$(echo ${ifline} | cut -f4 -d ':')"
-			nameserver="$(echo ${ifline} | cut -f5 -d ':')"
+		Parseipv4
 
 cat >> "${IFFILE}" << EOF
 allow-hotplug ${ifname}
@@ -54,49 +77,48 @@ iface ${ifname} inet static
     netmask ${ifnetmask}
 EOF
 
-			if [ -n "${ifgateway}" ]
-			then
+		if [ -n "${ifgateway}" ]
+		then
 
 cat >> "${IFFILE}" << EOF
     gateway ${ifgateway}
 
 EOF
 
-			fi
-
-			if [ -n "${nameserver}" ]
-			then
-				if [ -e "${DNSFILE}" ]
-				then
-					grep -v ^nameserver "${DNSFILE}" > "${DNSFILE}.tmp"
-					mv "${DNSFILE}.tmp" "${DNSFILE}"
-				fi
-
-				echo "nameserver ${nameserver}" >> "${DNSFILE}"
-			fi
-		done
-	else
-		if [ -n "${NODHCP}" ]
-		then
-			# force DHCP off
-			method="manual"
-		elif [ -z "${NETBOOT}" ] || [ -n "${DHCP}" ]
-		then
-			# default, dhcp assigned
-			method="dhcp"
-		else
-			# make sure that the preconfigured interface would not get reassigned by dhcp
-			# on startup by ifup script - otherwise our root fs might be disconnected!
-			method="manual"
 		fi
 
-		# iterate the physical interfaces and add them to the interfaces list and also add when ethdevice= called on cmdline
-		if [ "${method}" != dhcp ] || ([ ! -x /root/usr/sbin/NetworkManager ] && [ ! -x /root/usr/sbin/wicd ]) || [ ! -z "${ETHDEVICE}" ]
+		if [ -n "${nameserver}" ]
 		then
-			for interface in /sys/class/net/eth* /sys/class/net/ath* /sys/class/net/wlan* /sys/class/net/en*
-			do
-				[ -e ${interface} ] || continue
-				i="$(basename ${interface})"
+			if [ -e "${DNSFILE}" ]
+			then
+				grep -v ^nameserver "${DNSFILE}" > "${DNSFILE}.tmp"
+				mv "${DNSFILE}.tmp" "${DNSFILE}"
+			fi
+
+			echo "nameserver ${nameserver}" >> "${DNSFILE}"
+		fi
+else
+	if [ -n "${NODHCP}" ]
+	then
+		# force DHCP off
+		method="manual"
+	elif [ -z "${NETBOOT}" ] || [ -n "${DHCP}" ]
+	then
+		# default, dhcp assigned
+		method="dhcp"
+	else
+		# make sure that the preconfigured interface would not get reassigned by dhcp
+		# on startup by ifup script - otherwise our root fs might be disconnected!
+		method="manual"
+	fi
+
+	# iterate the physical interfaces and add them to the interfaces list and also add when ethdevice= called on cmdline
+	if [ "${method}" != dhcp ] || ([ ! -x /root/usr/sbin/NetworkManager ] && [ ! -x /root/usr/sbin/wicd ]) || [ ! -z "${ETHDEVICE}" ]
+	then
+		for interface in /sys/class/net/eth* /sys/class/net/ath* /sys/class/net/wlan* /sys/class/net/en*
+		do
+			[ -e ${interface} ] || continue
+			i="$(basename ${interface})"
 
 cat >> "${IFFILE}" << EOF
 allow-hotplug ${i}
@@ -104,18 +126,18 @@ iface ${i} inet ${method}
 
 EOF
 
-			done
-		fi
+		done
+	fi
 
-		if [ ! -f "${DNSFILE}" ] || [ -z "$(cat ${DNSFILE})" ]
+	if [ ! -f "${DNSFILE}" ] || [ -z "$(cat ${DNSFILE})" ]
+	then
+		if [ -f /netboot.config ]
 		then
-			if [ -f /netboot.config ]
-			then
-				# create a resolv.conf if it is not present or empty
-				cp /netboot.config /root/var/log/netboot.config
+			# create a resolv.conf if it is not present or empty
+			cp /netboot.config /root/var/log/netboot.config
 
-				rc_search=$(cat netboot.config | awk '/domain/ { print $3 }')
-				rc_server0="$(cat netboot.config | awk '/dns0/ { print $5 }')"
+			rc_search=$(cat netboot.config | awk '/domain/ { print $3 }')
+			rc_server0="$(cat netboot.config | awk '/dns0/ { print $5 }')"
 
 cat > $DNSFILE << EOF
 search ${rc_search}
@@ -123,17 +145,17 @@ domain ${rc_search}
 nameserver ${rc_server0}
 EOF
 
-				rc_server1=$(cat netboot.config | awk '/dns0/ { print $8 }')
+			rc_server1=$(cat netboot.config | awk '/dns0/ { print $8 }')
 
-				if [ "${rc_server1}" != "0.0.0.0" ]
-				then
-					echo "nameserver ${rc_server1}" >> $DNSFILE
-				fi
-
-				cat $DNSFILE >> /root/var/log/netboot.config
+			if [ "${rc_server1}" != "0.0.0.0" ]
+			then
+				echo "nameserver ${rc_server1}" >> $DNSFILE
 			fi
+
+			cat $DNSFILE >> /root/var/log/netboot.config
 		fi
 	fi
+fi
 
-	log_end_msg
+log_end_msg
 }
